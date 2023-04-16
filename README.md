3969323fa1f1fe571396b3a9cd39fa08a2457a1d
marisa2222 <msharifi2222@gmail.com>
2023-04-16 23:32:20 +0200

    added 32 files

---
+++ README.md
@@ -0,0 +1,38 @@
+# React Shopping Cart
+
+### [Demo Link](https://react-shopping-cart-project.netlify.app/) 🔗
+
+**Shopping Cart Project** built with **React JS**, using **Context API** & **useReducer Hook**.
+
+<br/>
+
+## Features:
+
+- #### Add to Cart
+- #### Remove from Cart
+- #### Increment / Decrement the Quantity
+
+<br/>
+
+## Technologies used:
+
+- #### **React JS**
+- #### **SASS**
+
+<br/>
+
+## Author:
+
+- **Gulshan Songara** - **[Portfolio Website](https://gulshansongara.netlify.app)**, **[Linkedin](https://www.linkedin.com/in/gulshan-songara/)**
+
+<br/>
+
+## Available Scripts:
+
+#### `npm start`
+
+#### `npm test`
+
+#### `npm run build`
+
+#### `npm run eject`
---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href=""
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        
+                    </a>
+
+                    <p>
+                        Built by | &nbsp;
+                        <a
+                            href=""
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            April 2023
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
