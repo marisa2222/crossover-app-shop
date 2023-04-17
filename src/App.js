98243d52794d1256b36b7b0d1992e9ae46a250ea
marisa2222 <msharifi2222@gmail.com>
2023-04-17 16:32:48 +0200

    modified App.js in src

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import { CartProvider } from "./context/cartContext";
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 import { Link, Routes, Route } from "react-router-dom";
-import LoginPage from './components/Login';
-import SignupPage from './components/Signup';
-
-
+import LoginPage from "./components/Login";
+import SignupPage from "./components/Signup";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" 
-        element={     
-        <CartProvider>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="checkout" element={<Checkout />} />
-          </Routes>
-        </CartProvider>
-      }/> 
-      <Route path="user/login" element={<LoginPage />} />
-      <Route path="user/signUp" element={<SignupPage />} />
-    </Routes>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="user/login" element={<LoginPage />} />
+          <Route path="user/signUp" element={<SignupPage />} />
+        </Routes>
+      </CartProvider>
     </>
   );
 }
